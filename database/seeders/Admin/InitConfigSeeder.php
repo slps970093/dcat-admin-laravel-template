88bd0f509e27903f55dc7dbc74dfff84e860061c
小周 <slps970093@gmail.com>
2022-11-30 09:43:51 +0800

    [初始化] 補上遺失的權限設定
    1. 初始化最高管理者權限
    2. 角色目錄 權限初始化

--- database/seeders/Admin/InitConfigSeeder.php
+++ database/seeders/Admin/InitConfigSeeder.php
@@ -24,37 +24,78 @@ class InitConfigSeeder extends Seeder
         /** @var \Illuminate\Database\Eloquent\Model $modelPermission */
         $modelPermission = app(config('admin.database.permissions_model'));
 
-        # 初始化一整批
-        $modelUser->where('is_superuser', '=', false)->update(['is_superuser' => true]);
-        $modelMenu->where('is_superuser', '=', false)
-            ->where('id', '>=', 2)
-            ->update(['is_superuser' => true]);
-        $modelRole->where('is_superuser', '=', false)->update(['is_superuser' => true]);
-        $modelPermission->where('is_superuser', '=', false)->update(['is_superuser' => true]);
+        try {
+            \DB::beginTransaction();
+            # 初始化一整批
+            $modelUser->where('is_superuser', '=', false)->update(['is_superuser' => true]);
+            $modelMenu->where('is_superuser', '=', false)
+                ->where('id', '>=', 2)
+                ->update(['is_superuser' => true]);
+            $modelRole->where('is_superuser', '=', false)->update(['is_superuser' => true]);
+            $modelPermission->where('is_superuser', '=', false)->update(['is_superuser' => true]);
 
-        # 增加新選單
+            # 增加新選單
+            $numCount = $modelMenu->count();
+            $rootMenu = $modelMenu->create([
+                'title' => '管理者',
+                'icon'  => 'fa-user',
+                'order' => $numCount++
+            ]);
 
-        $numCount = $modelMenu->count();
-        $rootMenu = $modelMenu->create([
-            'title' => '管理者',
-            'icon'  => 'fa-user',
-            'order' => $numCount++
-        ]);
+            $modelMenu->create([
+                'title'     => '用戶',
+                'parent_id' => $rootMenu->id,
+                'icon'      => 'fa-user',
+                'uri'       => 'employee/user',
+                'order'     => $numCount++
+            ]);
 
-        $modelMenu->create([
-            'title'     => '用戶',
-            'parent_id' => $rootMenu->id,
-            'icon'      => 'fa-user',
-            'uri'       => 'employee/user',
-            'order'     => $numCount++
-        ]);
+            $modelMenu->create([
+                'title'     => '角色',
+                'parent_id' => $rootMenu->id,
+                'icon'      => 'fa-users',
+                'uri'       => 'employee/role',
+                'order'     => $numCount++
+            ]);
 
-        $modelMenu->create([
-            'title'     => '角色',
-            'parent_id' => $rootMenu->id,
-            'icon'      => 'fa-users',
-            'uri'       => 'employee/role',
-            'order'     => $numCount++
-        ]);
+            # 增加新權限
+            $numCount = $modelPermission->count();
+
+            $rootPermission = $modelPermission->create([
+                'name'  => '後台管理',
+                'slug'  => '後台管理',
+                'order' => $numCount++
+            ]);
+
+            $modelPermission->create([
+                'name'      => '用戶管理',
+                'slug'      => '用戶管理',
+                'order'     => $numCount++,
+                'http_path' => '/employee/user*',
+                'parent_id' => $rootPermission->id
+            ]);
+
+            $modelPermission->create([
+                'name'      => '角色管理',
+                'slug'      => '角色管理',
+                'order'     => $numCount++,
+                'http_path' => '/employee/role*',
+                'parent_id' => $rootPermission->id
+            ]);
+
+            # 初始化最高管理者角色權限
+            $adminRole = $modelRole->where('slug', '=', 'administrator')->first();
+            $adminMenu = $modelMenu->where('is_superuser', '=', true)->get();
+            foreach ($adminMenu as $item) {
+                \DB::table(config('admin.database.role_menu_table'))->insert([
+                    'role_id' => $adminRole->id,
+                    'menu_id' => $item->id
+                ]);
+            }
+            \DB::commit();
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            echo "失敗 錯誤訊息 {$e->getMessage()}";
+        }
     }
 }
